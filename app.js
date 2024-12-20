6d3e7c9b7cb42bd208ee9dc1fc8298765240bea2
miljab <jablonskimilosz1337@gmail.com>
2024-12-20 14:14:28 +0100

    Basic app.js setup

---
+++ app.js
@@ -0,0 +1,10 @@
+const path = require("node:path");
+const express = require("express");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
